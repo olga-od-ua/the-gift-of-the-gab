94993b2e86f78fee82ccfa7c4bfc585541d3b5bf
olga-od-ua <olgasokolova21@gmail.com>
2021-06-17 21:38:43 +0000

    Add effect to Contact Us text

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.contact-info').mouseenter(function() {
+    $(this).addClass('add-border').addClass('change-text');
+});
+
+$('.contact-info').mouseleave(function() {
+    $(this).removeClass('add-border').removeClass('change-text');
+});
