b34bb5435951e321818c31ec4cc56d9dd4cd154b
olga-od-ua <olgasokolova21@gmail.com>
2021-06-20 11:28:29 +0000

    Fix the reset form function bug

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,3 +1,5 @@
+
+
 function sendMail(contactForm) {
     emailjs.send("service_r0g6ngj", "template_ddggyvm", {
 "user_email": contactForm.email.value,
@@ -5,18 +7,16 @@ function sendMail(contactForm) {
 "the_gift_of_the_gab_message": contactForm.message.value
     })
     .then(
-        function clearForm() {
-            document.getElementById("myForm").reset();
-         }
-    )
-    .then(
         function(response) {
             console.log("SUCCESS", response);
         },
         function(error) {
             console.log("FAILED", error);
-        }
-    )
+        })
+    .then (
+        function() {
+            document.getElementById("myForm").reset(); 
+        }) 
     return false;
 }
 
