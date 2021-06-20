596f834ea021d0c96d7c17d977bdedf8e483507f
olga-od-ua <olgasokolova21@gmail.com>
2021-06-20 16:45:34 +0000

    Add Copy button to mail address, its styling and functionality

--- js/index.js
+++ js/index.js
@@ -6,3 +6,18 @@ $('.contact-info').mouseleave(function() {
     $(this).removeClass('add-border').removeClass('change-text');
 });
 
+
+function handleCopyTextFromParagraph() {
+  const body = document.getElementById('mail-address');
+  const paragraph = document.getElementById('mail-address');
+  const area = document.createElement('textarea');
+  body.appendChild(area);
+
+  area.value = paragraph.innerText;
+  area.select();
+  document.execCommand('copy');
+
+  body.removeChild(area);
+  alert("Copied to clipboard")
+}
+
