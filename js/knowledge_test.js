24554ca97e51d0592a061357c6b390077f350ed6
olga-od-ua <olgasokolova21@gmail.com>
2021-07-10 14:10:42 +0000

    Add correct answer count statement

--- js/knowledge_test.js
+++ js/knowledge_test.js
@@ -16,11 +16,21 @@ function checkAnswer(userAnswerId, correctAnswerId, resultId) {
   let userAnswer = document.getElementById(userAnswerId).value.trim();
   let correctAnswer = document.getElementById(correctAnswerId).value;
   let result = document.getElementById(resultId);
-  
+  let correctAnswerCount = 0;
+
   if (userAnswer == correctAnswer) {
     result.innerHTML = 'Correct!';
+    correctAnswerCount++;
   } else {
     document.getElementById('test-table').style.display = 'none';
   }
+
+  if (correctAnswerCount <= 5) {
+    document.getElementById('result-level-message').innerHTML = 'Your level is Beginner/Elementary';
+  } else (
+    alert("Please Try Again!")
+  )
 }
+
+
   
