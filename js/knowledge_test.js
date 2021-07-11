3ec5881d9d77dd0f66ff2e66b82edee394c79cbd
olga-od-ua <olgasokolova21@gmail.com>
2021-07-11 11:52:52 +0000

    Disable submit buttons after one click

--- js/knowledge_test.js
+++ js/knowledge_test.js
@@ -11,23 +11,29 @@
 
 var correctAnswerCount = 0;
 
-function checkAnswer(userAnswerId, correctAnswerId, resultId) {
+function checkAnswer(userAnswerId, correctAnswerId, resultId, submitButtonId) {
   
   
   let userAnswer = document.getElementById(userAnswerId).value.trim();
   let correctAnswer = document.getElementById(correctAnswerId).value;
   let result = document.getElementById(resultId);
+  let submitButton;
   
   
 
   if (userAnswer == correctAnswer) {
 
     result.innerHTML = 'Correct!';
+    submitButton = document.getElementById(submitButtonId).disabled = true;
+    submitButton = document.getElementById(submitButtonId).style.display = "none";
     correctAnswerCount += 1;
-  
+     if (correctAnswerCount == 25) {
+      document.getElementById('test-table').style.display = 'none';
+      document.getElementById('result-level').innerHTML = 'Advanced';
+     }
   } else {
     document.getElementById('test-table').style.display = 'none';
-    if (correctAnswerCount <=1 && correctAnswerCount <5) {
+    if (correctAnswerCount >=1 && correctAnswerCount <5) {
       document.getElementById('result-level').innerHTML = 'Beginner';
     } else if (correctAnswerCount >= 5 && correctAnswerCount <10) {
       document.getElementById('result-level').innerHTML = 'Elementary';
@@ -37,14 +43,10 @@ function checkAnswer(userAnswerId, correctAnswerId, resultId) {
       document.getElementById('result-level').innerHTML = 'Intermediate';
     } else if (correctAnswerCount >=21 && correctAnswerCount <25) {
       document.getElementById('result-level').innerHTML = 'Upper-Intermediate';
-    } else if (correctAnswerCount == 25) {
-      document.getElementById('result-level').innerHTML = 'Advanced';
     } else {
       alert("try again")
     }
   }
-
-  
 }
 
 
