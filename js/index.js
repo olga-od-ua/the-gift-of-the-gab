e60161c9d91b39e75b02a07207e6c5e69b03fc26
olga-od-ua <olgasokolova21@gmail.com>
2021-07-14 21:45:55 +0000

    Update countdown function: add semicolon

--- js/index.js
+++ js/index.js
@@ -136,18 +136,18 @@ function countdown() {
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
     // Result is output to the specific element
-    document.getElementById("days").innerHTML = days + "d "
-    document.getElementById("hours").innerHTML = hours + "h " 
-    document.getElementById("mins").innerHTML = minutes + "m " 
-    document.getElementById("secs").innerHTML = seconds + "s " 
+    document.getElementById("days").innerHTML = days + "d ";
+    document.getElementById("hours").innerHTML = hours + "h ";
+    document.getElementById("mins").innerHTML = minutes + "m ";
+    document.getElementById("secs").innerHTML = seconds + "s "; 
         
     // Display the message when countdown is over
     if (timeleft < 0) {
         clearInterval(myfunc);
-        document.getElementById("days").innerHTML = ""
-        document.getElementById("hours").innerHTML = "" 
-        document.getElementById("mins").innerHTML = ""
-        document.getElementById("secs").innerHTML = ""
+        document.getElementById("days").innerHTML = "";
+        document.getElementById("hours").innerHTML = ""; 
+        document.getElementById("mins").innerHTML = "";
+        document.getElementById("secs").innerHTML = "";
         document.getElementById("end").innerHTML = "TIME UP!!";
     }
     }, 1000);
